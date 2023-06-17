06ae7422b11b87768782d326c315328f4df9520b
chi-chun <justin11223350@gmail.coml>
2023-06-17 17:25:58 +0800

    finish the function of detect unregular cycle of demc

--- util/demc.py
+++ util/demc.py
@@ -8,6 +8,39 @@ import click
 from src.image_process.cycle import ExtractMulitCycle
 import pydicom
 import re
+import time 
+import json
+import numpy as np
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Custom encoder for numpy data types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+
+            return int(obj)
+
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+
+        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+            return {'real': obj.real, 'imag': obj.imag}
+
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+
+        elif isinstance(obj, (np.bool_)):
+            return bool(obj)
+
+        elif isinstance(obj, (np.void)): 
+            return None
+
+        return json.JSONEncoder.default(self, obj)
+
+
+
 
 @click.command()
 @click.argument('file_path',nargs=-1,type=click.Path(exists=True))
@@ -21,6 +54,9 @@ def main(file_path,output,fps,export_data):
     if export_data!='all' and export_data!='npy' and export_data!='avi':
         print("Error: The option of \'--export_data\' must be \'all\', \'npy\' or \'avi\'")
         return 
+
+    process_time = time.ctime()
+    demc_info = {"process_time":process_time,"process_file_num":0,"process_file_info":[]}
     
     for f in file_path:
 
@@ -37,63 +73,26 @@ def main(file_path,output,fps,export_data):
             return
 
         else:
-            if export_data=='all':
-                extract_multi_cycle.exportNpy(output)
-                extract_multi_cycle.exportAvi(output,fps=fps)
-            elif export_data=='npy':
-                extract_multi_cycle.exportNpy(output)
-            elif export_data=='avi':
-                extract_multi_cycle.exportAvi(output,fps=fps)
+
+            #if export_data=='all':
+            #    extract_multi_cycle.exportNpy(output)
+            #    extract_multi_cycle.exportAvi(output,fps=fps)
+            #elif export_data=='npy':
+            #    extract_multi_cycle.exportNpy(output)
+            #elif export_data=='avi':
+            #    extract_multi_cycle.exportAvi(output,fps=fps)
+            demc_info['process_file_num']+=1
+            demc_info['process_file_info'].append(extract_multi_cycle.exportExtractInfo())
                 
-            print()
-            
+    print(demc_info)
     
-    #if len(file_path)==1:
-    #    avi_file_search = re.compile(r'avi$')
-    #    avi = avi_file_search.search(output)
-    #    if avi==None:
-    #        raise ValueError('The name of output file must be xxx.avi')
-    #    else:
-    #        head, tail = os.path.split(output)
-    #        if not(os.path.isdir(head)) and head!='':
-    #            raise ValueError('The output directory not found!')
-    #        output_file = [output]
-    #        
-    #    
-    #
-    #elif len(file_path)>=2:
-    #    output_file=[]
-    #    for f in file_path:
-    #        head,tail= os.path.split(f)
-    #        if output=='output.avi':
-    #            output_file.append('./output_video/'+tail+'.avi')
-    #        else:
-    #            if os.path.isdir(output):
-    #                if output[-1]!='/':
-    #                    output_file.append(output+'/'+tail+'.avi')
-    #                else:
-    #                    output_file.append(output+tail+'.avi')
-    #                    
-    #            else:
-    #                raise FileNotFoundError('Output Directory Not Found!')
-
-    ##print(file_path)
-    #for i,f in enumerate(file_path):
-    #    dcm = pydicom.dcmread(f)
-    #    #rgb_array = getRgbArray(dcm)
-    #    #print(rgb_array.shape)
-    #    
-    #    if only4d==False:
-    #        if len(rgb_array.shape)!=4:
-    #            raise ValueError('The shape of array data not match. If you want to output only 4D array please add \'--only4d\'')
-    #        #array2avi(rgb_array[:,:,:,::-1],output_file[i],fps,fourcc)
-    #        print('Finish processing',f)
-
-    #    elif only4d==True:
-    #        print('====Output Only 4D array Mode==== ')
-    #        if len(rgb_array.shape)==4:
-    #            #array2avi(rgb_array[:,:,:,::-1],output_file[i],fps,fourcc)
-    #            print('Finish processing',f)
+    if output[-1]!='/':
+        output+='/'
+    
+    demc_info_file_path =  output+'demc_info.json'
+    with open(demc_info_file_path, 'w') as f:
+      f.write(json.dumps(demc_info, indent = 4,cls=NumpyEncoder))
+    
 
 
 if __name__ == '__main__':
