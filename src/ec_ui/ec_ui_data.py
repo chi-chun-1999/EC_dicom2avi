4d89bbf58247a79bc6663552dc491a657354dbfc
chi-chun <justin11223350@gmail.coml>
2023-07-11 11:32:39 +0800

    finish the FileTree

--- src/ec_ui/ec_ui_data.py
+++ src/ec_ui/ec_ui_data.py
@@ -24,3 +24,43 @@ class ECData():
 
     def getPath(self):
         return self._file_path
+    
+    @property
+    def file_name(self):
+        return self._file_name
+
+    @property
+    def file_path(self):
+        return self._file_path
+
+    def __str__(self) -> str:
+        name = 'ECData('+self.file_name+')'
+        return name
+    
+    def __repr__(self) -> str:
+        name = 'ECData('+self.file_name+')'
+        return name
+
+class DirData():
+    def __init__(self,dir_path,dir_name) -> None:
+        self.__dir_path = dir_path
+        self.__dir_name = dir_name
+    
+    @property
+    def dir_path(self):
+        return self.__dir_path
+
+    @property
+    def dir_name(self):
+        return self.__dir_name
+    
+    def __str__(self) -> str:
+        name = 'DirData('+self.dir_name+')'
+        return name
+    
+    def __repr__(self) -> str:
+        name = 'DirData('+self.dir_name+')'
+        return name
+    
+
+    
